0c853ac5c4dfecf6bfae1471f47e205ccef95680
Ole Kristian Losvik <losvik@gmail.com>
2017-09-10 20:30:34 +0200

    fix

--- functions.php
+++ functions.php
@@ -14,9 +14,13 @@ function my_theme_enqueue_styles() {
 	wp_dequeue_style( 'positor-bootstrap' );
 	wp_dequeue_style( 'positor-style' );
 
-	wp_enqueue_style( 'journalen-style',
+	wp_enqueue_style( 'journalen-bootstrap',
 		get_stylesheet_directory_uri() . '/assets/stylesheets/journalen.min.css',
 		wp_get_theme()->get( 'Version' )
 	);
+	wp_enqueue_style( 'journalen-style',
+	get_stylesheet_directory_uri() . '/style.css',
+	wp_get_theme()->get( 'Version' )
+);
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
