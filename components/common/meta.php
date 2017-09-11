540925121bbdff015a48fe69784ab210176e09bd
Ole Kristian Losvik <losvik@gmail.com>
2017-09-11 07:56:54 +0200

    upd

--- components/common/meta.php
+++ components/common/meta.php
@@ -6,10 +6,10 @@
  */
 
 ?>
-<div class="meta py-3">
+<div class="meta py-3 font-weight-300">
 <?php // Show author_alias if assigned
 if (get_post_meta(get_the_ID(), 'author_alias', true) != "") :
-    echo '<span class="sr-only">Skribent </span><i class="material-icons">person</i>&nbsp;' . get_post_meta(get_the_ID(), 'author_alias', true); 
+    echo '<span class="sr-only">Skribent </span><i class="material-icons align-middle">person</i>&nbsp;' . get_post_meta(get_the_ID(), 'author_alias', true); 
     echo '&nbsp;<small>' . get_post_meta(get_the_ID(), 'author_bio', true) . '</small>';
 
 endif;
@@ -17,11 +17,11 @@ endif;
 
 <?php // Show photographer_alias if assigned
 if (get_post_meta(get_the_ID(), 'photographer_alias', true) != "") :
-    echo '&nbsp;<span class="sr-only">Fotograf </span><i class="material-icons">photo_camera</i>&nbsp;' . get_post_meta(get_the_ID(), 'photographer_alias', true); 
+    echo '&nbsp;<span class="sr-only">Fotograf </span><i class="material-icons align-middle">photo_camera</i>&nbsp;' . get_post_meta(get_the_ID(), 'photographer_alias', true); 
 endif;
 ?>
 <?php // Show published date
-    echo '&nbsp;<i class="material-icons">date_range</i>&nbsp;Publisert ' . get_the_date('j.n.Y');
+    echo '&nbsp;<i class="material-icons align-middle">date_range</i>&nbsp;Publisert ' . get_the_date('j.n.Y');
     
     // If updated date is different, show this as well
     if ( get_the_date( 'j.n.Y' ) !== get_the_modified_date( 'j.n.Y' ) ) {
