d1dcc2f54caf0040eeb4e0a184ff015977588b85
Ole Kristian Losvik <losvik+github@gmail.com>
2017-10-16 18:14:44 +0200

    new color

--- components/common/archive-navigation.php
+++ components/common/archive-navigation.php
@@ -12,7 +12,7 @@
 	// Previous post.
 	if ( get_previous_posts_link() ) : ?>
 
-	<a href="<?php echo esc_url( get_previous_posts_page_link() ); ?>" rel="prev" title="Navigate to the newer posts" class="link-no-decoration btn btn-outline-primary">
+	<a href="<?php echo esc_url( get_previous_posts_page_link() ); ?>" rel="prev" title="Navigate to the newer posts" class="link-no-decoration btn btn-outline-danger">
 	<span class="fa fa-lg fa-chevron-left mx-2" aria-hidden="true"></span> 
 	<?php esc_html_e( 'Newer posts', 'positor' ); ?>
 	</a>
@@ -22,7 +22,7 @@
 	// Next post.
 	if ( get_next_posts_page_link() ) : ?>                                                                   
 	<div class="ml-auto">
-	<a href="<?php echo esc_url( get_next_posts_page_link() ); ?>" rel="next" title="Navigate to the older post" class="link-no-decoration btn btn-outline-primary ">
+	<a href="<?php echo esc_url( get_next_posts_page_link() ); ?>" rel="next" title="Navigate to the older post" class="link-no-decoration btn btn-outline-danger ">
 		<?php esc_html_e( 'Older posts', 'positor' ); ?>
 		<span class="fa fa-lg fa-chevron-right" aria-hidden="true"></span> 
 	</a>
